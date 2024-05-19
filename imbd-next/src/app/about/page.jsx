b80bc3872b88834d8751d08035a5bff56fb10cb1
hango <ngothithanhha2k@gmail.com>
2024-05-19 21:45:07 +0700

    Add about page

---
+++ imbd-next/src/app/about/page.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        The story follows writer and performer Richard Gadds warped relationship
+        with a female stalker and the impact it has on him as he is ultimately
+        forced to face a deep, dark buried trauma.
+      </p>
+      <p>
+        I couldnt help but cry at those scenes, the angst and disgust were
+        almost unbearable, and to think that there are some many evil people in
+        the world just like that makes my skin crawl. Watching Donnys processing
+        of what happened to him felt really familiar and accurate, as its often
+        really convoluted and confusing as it was depicted on the show. His
+        portrait of the shame aspect of it, as it takes a huge toll on the
+        victims life, was incredibly faithful and well-done.
+      </p>
+    </div>
+  );
+}
